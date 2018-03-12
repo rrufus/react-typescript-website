95de92ce55638d2a038d0652dbc67633880a8ffb
Rufus Russell <rufusrussell@uk.ibm.com>
2018-03-12 23:41:52 +0000

    update copy

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,7 +1,7 @@
 export const Intro = `I am a full stack software engineer and tech lead with a degree in Physics.`;
 
 export const MoreInfo = `I am experienced in leading small teams 
-through MVPs using the extreme programming methodology.`;
+through MVPs using Extreme Programming.`;
 
 export const FavouriteLang = 'My favourite language is golang.';
 
